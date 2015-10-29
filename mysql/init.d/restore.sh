4c24e13ebf904a5323ce8569449a9d5da5462328
Viktor Kovacs <kovvik@gmail.com>
2015-10-29 21:26:13 +0100

    Removes Wordpress dependencies

--- mysql/init.d/restore.sh
+++ mysql/init.d/restore.sh
@@ -4,5 +4,5 @@ DUMPFILE=dump.sql
 if [ -f $DUMPFILE ];
 then
   echo "Dump file found, restoring database from it..."
-  mysql --host=localhost --port=3306 -u root --protocol=tcp --password=1q9o2w8i37eu wordpress < $DUMPFILE && echo "Done."
+  mysql --host=localhost --port=3306 -u root --protocol=tcp --password=1q9o2w8i37eu database < $DUMPFILE && echo "Done."
 fi
